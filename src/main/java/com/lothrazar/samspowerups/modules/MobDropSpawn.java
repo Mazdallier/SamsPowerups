50bd436dd987f2535ab69fe24fc56510fab92ada
Sam Bassett <lothrazar@hotmail.com>
2014-11-01 10:49:41 -0700

    more module placeholders

---
+++ src/main/java/com/lothrazar/samspowerups/modules/MobDropSpawn.java
@@ -0,0 +1,26 @@
+package com.lothrazar.samspowerups.modules;
+
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+
+public class MobDropSpawn 
+{
+	/*
+	//TOOD: call this from the real core event, and then check the entity out and maybe drop stuff
+	public void onLivingDrops(LivingDropsEvent event)
+	{
+	 if (event.entityLiving.worldObj.isClient) {return; }
+	
+	
+	
+	if(event.entityLiving instanceof EntityWolf)
+	{
+	
+	}
+	
+		  event.entityLiving.entityDropItem(itemStack, 0.0F);
+			
+			
+			
+	}
+	*/
+}
--- src/main/java/com/lothrazar/samspowerups/modules/WorldGenSpawn.java
+++ src/main/java/com/lothrazar/samspowerups/modules/WorldGenSpawn.java
@@ -3,6 +3,7 @@ import cpw.mods.fml.common.IWorldGenerator;
 import java.util.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 
@@ -16,6 +17,20 @@ public class WorldGenSpawn implements IWorldGenerator
 			IChunkProvider chunkProvider) 
 	{
 	 
+		int nearbyX = chunkX + random.nextInt(16)+8;
+		int nearbyZ = chunkZ + random.nextInt(16)+8;
+		//TODO: switch on biome as well opr instead of the dimension?
+		//world.getBiomeGenForCoords(k, l) == BiomeGenBase.swampland
+		
+		//case statements must be constants
+		/*
+		switch(world.getBiomeGenForCoords(nearbyX,nearbyZ).biomeID )
+		{
+			case BiomeGenBase.swampland.biomeID:
+			break;
+		}
+		*/
+		
 		switch(world.provider.dimensionId )
 		{
 			case 0: //then we are on the overworld
