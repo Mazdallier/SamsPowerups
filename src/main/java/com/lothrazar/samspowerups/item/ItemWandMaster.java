29e63127aa93317c8c29de0d833d267d51fec29f
Sam Bassett <lothrazar@hotmail.com>
2014-11-02 15:16:00 -0800

    master wand with module

---
+++ src/main/java/com/lothrazar/samspowerups/item/ItemWandMaster.java
@@ -0,0 +1,53 @@
+package com.lothrazar.samspowerups.item;
+
+import com.google.common.collect.Sets;
+import com.lothrazar.samspowerups.ModCore;
+import com.lothrazar.samspowerups.util.Reference;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockChest;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntityMooshroom;
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.MathHelper;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+
+public class ItemWandMaster extends ItemTool
+{
+	private static int RADIUS = 128;
+	private static int DURABILITY = 50;
+ 
+	public ItemWandMaster( )
+	{   
+		super(1.0F,Item.ToolMaterial.WOOD, Sets.newHashSet()); 
+    	this.setMaxDamage(DURABILITY); 
+		this.setMaxStackSize(1);
+    	setCreativeTab(CreativeTabs.tabTools) ;   
+	}
+	 
+
+	private static int RADIUS_PROSPECT = 8;
+	
+
+	@Override
+    public boolean hasEffect(ItemStack par1ItemStack)
+    {
+    	return true; //give it shimmer
+    }
+}
---
+++ src/main/java/com/lothrazar/samspowerups/modules/MasterWandModule.java
@@ -0,0 +1,51 @@
+package com.lothrazar.samspowerups.modules;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+import com.lothrazar.samspowerups.BaseModule;
+import com.lothrazar.samspowerups.ModCore;
+import com.lothrazar.samspowerups.handler.WandHandler;
+import com.lothrazar.samspowerups.item.ItemWandMaster;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class MasterWandModule extends BaseModule
+{ 
+	public static ItemWandMaster itemWand;
+	public MasterWandModule()
+	{
+		Handler = new WandHandler();
+	}
+	
+	@Override
+	public void loadConfig() { 
+	}
+
+	@Override
+	public String getName() { 
+		return "Master Wand";
+	}
+
+	@Override
+	public void init() 
+	{  
+		itemWand = new ItemWandMaster();
+		itemWand.setUnlocalizedName("wand_master").setTextureName(ModCore.MODID+":wand_master");
+		GameRegistry.registerItem(itemWand,  "wand_master");   
+		GameRegistry.addRecipe(new ItemStack(itemWand)
+			,"bdb"
+			," b "
+			," b "
+			, 'd', Blocks.emerald_block 
+			, 'b', Items.blaze_rod  );
+		GameRegistry.addSmelting(itemWand, new ItemStack(Blocks.emerald_block,1,0),0);	//recycling	 
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
