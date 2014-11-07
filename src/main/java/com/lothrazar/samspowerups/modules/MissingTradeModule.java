566dd3914d8cbb456ad588481b968047b4ec148d
Sam Bassett <lothrazar@hotmail.com>
2014-11-06 20:05:22 -0800

    missing villager registry entry

--- src/main/java/com/lothrazar/samspowerups/modules/MissingTradeModule.java
+++ src/main/java/com/lothrazar/samspowerups/modules/MissingTradeModule.java
@@ -3,12 +3,15 @@ package com.lothrazar.samspowerups.modules;
 import com.lothrazar.samspowerups.BaseModule;
 import com.lothrazar.samspowerups.handler.VillagerTradeHandler;
 
+import cpw.mods.fml.common.registry.VillagerRegistry;
+
 public class MissingTradeModule extends BaseModule 
 {
 
 	public MissingTradeModule()
 	{
-		Handler = new VillagerTradeHandler();
+		//Handler =new VillagerTradeHandler() ;
+		Name="Missing Trades added";
 	}
 	@Override
 	public void loadConfig() { 
@@ -16,8 +19,11 @@ public class MissingTradeModule extends BaseModule
 	}
 
 	@Override
-	public void init() { 
-		
+	public void init() 
+	{ 
+		VillagerTradeHandler handler = new VillagerTradeHandler();
+		VillagerRegistry.instance().registerVillageTradeHandler(1, handler);
+		VillagerRegistry.instance().registerVillageTradeHandler(2, handler);
 	}
 
 	@Override
