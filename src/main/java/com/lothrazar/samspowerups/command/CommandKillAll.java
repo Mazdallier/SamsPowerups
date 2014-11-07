7ff139fe5f8096021bd76b01ca351f2c8128d80f
Sam Bassett <lothrazar@hotmail.com>
2014-11-06 23:26:35 -0800

    command works now killall hits creepers iwthin range

--- src/main/java/com/lothrazar/samspowerups/command/CommandKillAll.java
+++ src/main/java/com/lothrazar/samspowerups/command/CommandKillAll.java
@@ -3,10 +3,15 @@ package com.lothrazar.samspowerups.command;
 import java.util.List;
 
 import com.lothrazar.samspowerups.util.Chat;
+import com.sun.xml.internal.stream.Entity;
 
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 
 public class CommandKillAll implements ICommand
 {
@@ -35,13 +40,36 @@ public class CommandKillAll implements ICommand
 	{ 
 
 		  EntityPlayer p = (EntityPlayer)ic;
-
+		  int range = 50;
+		  double px = p.posX;
+			double py = p.posY;
+			double pz = p.posZ;
+			
 		  Chat.addMessage(p,"killall");
+		  
+		//  List c =   p.worldObj.loadedEntityList; 
+		  
+		  
+		  List<EntityCreeper> list = p.worldObj.getEntitiesWithinAABB(
+					EntityCreeper.class, 
+					AxisAlignedBB.getBoundingBox(px - range, py - range, pz - range,
+												 px + range, py + range, pz + range));
+
+		  
+		  int killed = 0;
+		   
+		  for(EntityCreeper e : list)
+		  {
+			  if(e.isDead) {continue;}
+	  
+			  e.attackEntityFrom(DamageSource.magic, 33);
+			  killed++;
+		  }
 	}
 
 	@Override
 	public boolean canCommandSenderUseCommand(ICommandSender ic) { 
-		return false;
+		return true;
 	}
 
 	@Override
