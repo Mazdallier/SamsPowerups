824301e601d1a125a50017e66150397189027c8f
Sam Bassett <lothrazar@hotmail.com>
2014-11-12 09:57:56 -0800

    next goal: perfect expolosions, no destroyed items or entiteis

---
+++ src/main/java/net/minecraft/world/Explosion.java
@@ -0,0 +1,7 @@
+package net.minecraft.world;
+
+public class Explosion {
+//TODO: 1>  how to we make explosions drop 100% of items instead of a fraction?
+	
+	//TODO 2> make explosions NOT kill entity items, such as from a broken chest
+}
