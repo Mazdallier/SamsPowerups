2922c3aed0c9860906165d5ca50d317e522c53d1
Sam Bassett <lothrazar@hotmail.com>
2014-12-11 00:23:36 -0800

    merging mods for easier deployment

--- src/main/java/com/lothrazar/samscommands/SamsCommandsMod.java
+++ src/main/java/com/lothrazar/samscommands/SamsCommandsMod.java
@@ -5,20 +5,32 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;  
+import java.util.Random;
+
 import org.apache.logging.log4j.Logger; 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.util.MathHelper;
+import net.minecraft.village.Village;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.passive.EntityHorse;
 import net.minecraftforge.common.DimensionManager;  
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration; 
 
 import com.lothrazar.samsenderpower.CommandEnderChest;
 import com.lothrazar.util.Location; 
+import com.lothrazar.util.Reference;
+
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -37,7 +49,12 @@ public class SamsCommandsMod
     public static Logger logger;  
 	public static Configuration config;  
     protected static final String MODID = "samscommands"; 
-    public static final String VERSION = "1";
+    public static final String VERSION = "1";	
+    private boolean showDefaultDebug = true ; //TODO: split to left and right
+	private boolean showGameRules = true;
+	private boolean showSlimeChunk = true;
+	private boolean showVillageInfo = true; 
+	private boolean showHorseInfo = true;
 	public void syncConfig() 
 	{
 		if(config.hasChanged()) { config.save(); } 
@@ -51,6 +68,7 @@ public class SamsCommandsMod
 			instance.syncConfig();
 		} 
     }
+    
     @EventHandler
     public void onPreInit(FMLPreInitializationEvent event)   
     {  
@@ -59,41 +77,52 @@ public class SamsCommandsMod
     	config = new Configuration(event.getSuggestedConfigurationFile());  
 		
 		boolean enabled = config.getBoolean( "richLoot",MODID,true,
-				"More goodies in dungeon chests (all chests in the game except for starter chest and dungeon dispensers): emeralds, quartz, glowstone, pistons, gold blocks, records, TNT, anvils."
+				"More goodies in dungeon chests (all chests in the game except for starter " +
+				"chest and dungeon dispensers): emeralds, quartz, glowstone, pistons, " +
+				"gold blocks, records, TNT, anvils."
 		);
 		
 		syncConfig() ;
 
 	    MinecraftForge.EVENT_BUS.register(instance); 
     } 
-    
-
-  
-  
-	  
+     
     @EventHandler
 	public void onServerLoad(FMLServerStartingEvent event)
 	{ 
-		event.registerServerCommand(new CommandSearchTrades());
-		logger.info("Register searchtrade command");
-		event.registerServerCommand(new CommandSearchItem());
-		logger.info("Register searchitem command");
-		event.registerServerCommand(new CommandKillAll());
-		logger.info("Register killall command");
-		event.registerServerCommand(new CommandSimpleWaypoints());
-		logger.info("Register simplewaypoint command");
-		event.registerServerCommand(new CommandTodoList()); 
+		event.registerServerCommand(new CommandSearchTrades()); 
+		event.registerServerCommand(new CommandSearchItem()); 
+		event.registerServerCommand(new CommandKillAll()); 
+		event.registerServerCommand(new CommandSimpleWaypoints()); 
+		event.registerServerCommand(new CommandTodoList());  
 	}
-	
 
+  
+	 
+	
 	@SubscribeEvent
 	public void onRenderTextOverlay(RenderGameOverlayEvent.Text event)
 	{
 		if(Minecraft.getMinecraft().gameSettings.showDebugInfo == false){return;}
+		 
+		//config file can disable all this, which keeps the original screen un-cleared
+		if(showDefaultDebug == false)
+		{
+			event.left.clear();
+			event.right.clear();
+		}
 		
+		AddLeftInfo(event.left);
+		AddRightInfo(event.right); 
+		
+		AddWaypointInfo(event); 
+	}
+
+	private void AddWaypointInfo(RenderGameOverlayEvent.Text event) 
+	{
 		EntityClientPlayerMP p = Minecraft.getMinecraft().thePlayer;
 	 
-    	ArrayList<String> saved = SamsCommandsMod.GetForPlayerName(Minecraft.getMinecraft().thePlayer.getDisplayName());
+    	ArrayList<String> saved = GetForPlayerName(Minecraft.getMinecraft().thePlayer.getDisplayName());
 
     	if(saved.size() > 0 && saved.get(0) != null)
     	{ 
@@ -140,10 +169,270 @@ public class SamsCommandsMod
     				event.left.add(showName);
     		} 
     	}
-    
 	}
 	
+ 
+	 
+	private void AddLeftInfo(ArrayList<String> side)
+	{  
+		//the current client side player, not SMP
+		EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer; 
+		World world = Minecraft.getMinecraft().getIntegratedServer().getEntityWorld();
+    	   
+		long ticksPerDay = 24000 ;
 	
+		long time = world.getWorldTime(); 
+	 
+		int days = MathHelper.floor_double( time / ticksPerDay);
+		 
+		long remainder = time % ticksPerDay;
+		
+		String detail="";
+	
+		if(remainder < 5000) detail="Morning";
+		else if(remainder < 7000) detail="Mid-day";//midd ay is exactly 6k, so go one on each side
+		else if(remainder < 12000) detail="Afternoon";
+		else detail = "Night"; //  (Moon Phase" + world.getMoonPhase()+")??
+	  
+		side.add("Day "+days +" ("+detail+")");  
+		
+		
+		//inspired by : http://www.minecraftforge.net/forum/index.php?topic=6514.0
+	 	int yaw = (int)player.rotationYaw ;
+	 	if(yaw < 360) yaw += 360;//this SEEMS LIKE it doesnt matter, since we do Math.abs
+	 	//BUT, by doing that then adding the 22, it fixes th half widths
+		yaw += 22;//  magic fix number so SE and SW are balanced for example
+		//if we dont do this 22 fix, then south east and south west will NOT be at 45 deg, they will 
+		//not be equidistant from SOUTH 
+		
+	 	int f = Math.abs( (yaw %= 360) /45);   //  360degrees divided by 45 == 8 zones
+	
+		 
+		String facing = "";
+		switch(f)
+		{
+			case 0: facing = "South";break;
+			case 1: facing = "South-East";break; 
+			case 2: facing = "East";break;
+			case 3: facing = "North-East";break;
+			case 4: facing = "North";break;
+			case 5: facing = "North-West";break;
+			case 6: facing = "West";break;
+			case 7: facing = "South-West";break;
+			default: facing = ""+f;//debug any mistakes
+		}
+			  
+		side.add(facing); 
+		 
+		if(showDefaultDebug == false)
+		{
+			//only show this part if we are hiding the vanilla
+			//since Y and biome are already in that part
+			 
+			BiomeGenBase biome = world.getBiomeGenForCoords((int)player.posX, (int)player.posZ); 
+			String biomeDetails = biome.biomeName;// +" (Temperature "+biome.temperature+")";
+			
+			side.add(biomeDetails);
+			side.add("Height " +MathHelper.floor_double(player.posY)); 
+			//side.add("");
+			side.add(Minecraft.getMinecraft().renderGlobal.getDebugInfoEntities());
+			 
+		}
+  
+	 	if(showSlimeChunk && player.dimension == 0)
+	 	{ 
+	    	long seed =  world.getSeed();
+	  	
+	    	Chunk in = world.getChunkFromBlockCoords(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posZ));
+
+			//formula source : http://minecraft.gamepedia.com/Slime
+	    	Random rnd = new Random(seed +
+	                (long) (in.xPosition * in.xPosition * 0x4c1906) +
+	                (long) (in.xPosition * 0x5ac0db) + 
+	                (long) (in.zPosition * in.zPosition) * 0x4307a7L +
+	                (long) (in.zPosition * 0x5f24f) ^ 0x3ad8025f);
+	    	
+			boolean isSlimeChunk = (rnd.nextInt(10) == 0);
+	     
+			if(isSlimeChunk)
+			{
+	    		side.add("Slime Chunk"); 
+			} 
+	 	}
+	 	
+	 	if(showVillageInfo && world.villageCollectionObj != null)
+	 	{  
+	 
+			 int playerX = MathHelper.floor_double(player.posX);
+			 int playerY = MathHelper.floor_double(player.posY);
+			 int playerZ = MathHelper.floor_double(player.posZ);
+			 
+			 int dX,dZ;
+			 
+			 int range = 10;
+			 Village closest = world.villageCollectionObj.findNearestVillage(playerX, playerY, playerZ, range);
+		     
+			 if(closest != null)
+			 { 
+			    int doors = closest.getNumVillageDoors();
+			    int villagers = closest.getNumVillagers();
+			    int rep = closest.getReputationForPlayer(player.getCommandSenderName());
+	 
+			    //int golem_limit = MathHelper.floor_double(villagers / 10); 
+			    //boolean mating = closest.isMatingSeason();
+
+			    side.add("");
+			    side.add("Village Data");
+			    side.add(String.format("# of Villagers: %d",villagers));
+			    side.add(String.format("Reputation: %d",rep));
+			    side.add(String.format("Valid Doors: %d",doors));
+
+			   // side.add(String.format("center coords: %d  %d",closest.getCenter().posX,closest.getCenter().posZ));
+			    
+			    dX = playerX - closest.getCenter().posX;
+			    dZ = playerZ - closest.getCenter().posZ;
+			    
+			    int dist = MathHelper.floor_double(Math.sqrt( dX*dX + dZ*dZ));
+
+			    side.add(String.format("Distance from Center:  %d", dist));
+			    
+			 }	 
+		 }
+	 	
+	 	if(showHorseInfo && player.ridingEntity != null)
+	 	{
+	 		if(player.ridingEntity instanceof EntityHorse)
+	 		{
+	 			EntityHorse horse = (EntityHorse)player.ridingEntity;
+	 			
+	 			//horse.getCreatureAttribute().values()[0].
+	 			
+	 			//int type = horse.getHorseType();
+	 			//type 0 is horse, type 1 is donkey, type 2 is mule
+	 			
+	 			 
+	 			//variant 0 is white, 1 is creamy
+	 			//2 is chestnut 3 is brown
+	 			//4 is black, 5 is grey, 6 is dark brown
+	 			
+	 			//int armor = horse.getTotalArmorValue();
+	 			
+	 			// nope not this:float speed = horse.getAIMoveSpeed();
+	 			double speed =  horse.getEntityAttribute(SharedMonsterAttributes.movementSpeed).getAttributeValue() ;
+	 				 
+	 			double jump = horse.getHorseJumpStrength() ;
+	 			//convert from scale factor to blocks
+	 			double jumpHeight = 0;
+	 			double gravity = 0.98;
+	 			while (jump > 0)
+	 			{
+		 			jumpHeight += jump;
+		 			jump -= 0.08;
+		 			jump *= gravity;
+	 			}
+	 		
+	 			//http://minecraft.gamepedia.com/Item_id#Horse_Variants
+	 			String variant = "";
+	 			 
+	 			int var = horse.getHorseVariant();
+
+	 			String spots = null;
+	 			
+	 			if(var >= 1024) spots = "Black Dots ";
+	 			else if(var >= 768) spots = "White Dots";
+	 			else if(var >= 512) spots = "White Field";
+	 			else if(var >= 256) spots = "White Patches";
+	 			
+	 			while(var - 256 > 0)
+	 			{
+	 				var -= 256;
+	 			}
+	 			
+	 			switch( var )
+	 			{
+		 			case 0:  variant = "White";break; 
+		 			case 1: variant = "Creamy";break;
+		 			case 2: variant = "Chestnut";break;
+		 			case 3: variant = "Brown";break;
+		 			case 4: variant = "Black";break;
+		 			case 5: variant = "Gray";break;
+		 			case 6: variant = "Dark Brown";break; 
+	 			}
+	 			
+	 			//if its not a horse, variant wont matter
+	 			String type = "";
+	 			switch( horse.getHorseType())
+	 			{
+		 			case 0: type = variant + " Horse";break;
+		 			case 1: type = "Donkey";break;
+		 			case 2: type = "Mule";break;
+		 			case 3: type = "Undead Horse";break;
+		 			case 4: type = "Skeleton Horse";break;
+	 			}
+
+	 			if(spots != null) type += " ("+spots+")";
+
+	 			side.add("");
+	 			side.add("Riding "+type); 
+
+	 			DecimalFormat df = new DecimalFormat("0.0000");
+	 			
+	 			side.add("  "+ df.format(speed) + " Speed"  ); 
+	 			
+	 			df = new DecimalFormat("0.0");
+	 			
+	 			side.add("  "+ df.format(jumpHeight) + " Jump"  ); 
+	 			
+	 		}
+	 	} 
+	}
+
+	private void AddRightInfo(ArrayList<String> side)
+	{ 
+		World world = Minecraft.getMinecraft().getIntegratedServer().getEntityWorld();
+		EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer; 
+ 
+		if(showGameRules)
+		{ 
+			side.add("Enabled Gamerules:");
+			
+			GameRules rules = world.getWorldInfo().getGameRulesInstance();
+			
+			ArrayList<String> ruleNames = new ArrayList<String>();
+			ruleNames.add(Reference.gamerule.commandBlockOutput);
+			ruleNames.add(Reference.gamerule.doDaylightCycle);
+			ruleNames.add(Reference.gamerule.doFireTick);
+			ruleNames.add(Reference.gamerule.doMobLoot);
+			ruleNames.add(Reference.gamerule.doMobSpawning);
+			ruleNames.add(Reference.gamerule.doTileDrops);
+			ruleNames.add(Reference.gamerule.keepInventory);
+			ruleNames.add(Reference.gamerule.mobGriefing);
+			ruleNames.add(Reference.gamerule.naturalRegeneration);
+
+			String name;
+			for(int i = 0; i < ruleNames.size(); i++)
+			{
+				name = ruleNames.get(i);
+				if(rules.getGameRuleBooleanValue(name))
+				{ 
+					side.add(name); 
+				}
+			} 
+		} 
+		  
+		String todoCurrent = CommandTodoList.GetTodoForPlayerName(player.getDisplayName());
+		
+		if(todoCurrent != null && todoCurrent.isEmpty() == false)
+		{
+			side.add("");
+			side.add("TODO : "+todoCurrent); 
+		}  
+	} 
+
+
+	
+	
+	 
 	public static ArrayList<String> GetForPlayerName(String playerName)
 	{ 
 		if(playerName == null)
