80b0727bde591687421de716294ce8ea0d2238d4
Sam Bassett <lothrazar@hotmail.com>
2014-12-28 21:13:04 -0800

    prep for new area: 1.8 features

---
+++ src/main/java/com/lothrazar/backport/BackportedFeatures.java
@@ -0,0 +1,28 @@
+package com.lothrazar.backport;
+
+public class BackportedFeatures {
+/*uncrafting
+powered rail back into gold
+
+runestones
+remove negatives
+config file for location 
+(bottom right for example. do all four corners)
+
+idea:
+1.8 features?
+Blocks:
+Diorite, Andesite, Granite, Polished Diorite, Polished Andesite, Polished Granite, Coarse Dirt, Red Sandstone, Prismarine, Prismarine Bricks, Dark Prismarine, Sea Lantern, Prismarine Crystals, Prismarine Shard, Wet Sponge, Slime Block
+5 new doors
+5 new fences
+Items:
+Rabbit's Foot, New Fences, New Gates, Iron Trapdoor, Armor Stand
+Food:
+Raw Rabbit, Cooked Rabbit, Rabbit Stew, Raw Mutton, Cooked Mutton
+Potions:
+Potion of Leaping
+Enchantment:
+Depth Strider
+Mobs:
+Endermites, Guardians, Elder Guardians, Rabbits*/
+}
