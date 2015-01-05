5d7526a1be13576914bfb0800b35ed820cd8d882
Sam Bassett <lothrazar@hotmail.com>
2015-01-05 10:08:53 -0800

    player death: put everything in the chest, until the chest is full

--- src/main/java/com/lothrazar/samscontent/HandlerPlayerDeath.java
+++ src/main/java/com/lothrazar/samscontent/HandlerPlayerDeath.java
@@ -1,7 +1,10 @@
 package com.lothrazar.samscontent;
 
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.event.entity.player.PlayerDropsEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -17,8 +20,19 @@ public class HandlerPlayerDeath
 		ItemStack itemStack;
 		
 		//if gamerule keepInventory is true, drops is just an empty array so this does nothing anyway
+		//or maybe you died holding nothing at all
+		if(event.drops.size() == 0) { return; }
 		
+		//TODO: find a chest in their inventory and use that, minus it by one. or else dont do this?
 		
+		int x = MathHelper.floor_double(event.entityPlayer.posX);
+		int y = MathHelper.floor_double(event.entityPlayer.posY);
+		int z = MathHelper.floor_double(event.entityPlayer.posZ);
+		
+		event.entityPlayer.worldObj.setBlock(x, y, z, Blocks.chest, 0, 2);
+		TileEntityChest chest = (TileEntityChest) event.entityPlayer.worldObj.getTileEntity(x, y, z);
+		
+		int slot = 0;
 		for(EntityItem dropped : event.drops) 
 		{
 			if(dropped == null) { continue; }
@@ -26,6 +40,16 @@ public class HandlerPlayerDeath
 			if(itemStack == null) { continue; }
 			
 			System.out.println(itemStack.getUnlocalizedName());
+			
+			if(slot <= chest.getSizeInventory())
+			{ 
+				chest.setInventorySlotContents(slot, itemStack);
+				
+				slot++;
+				
+				dropped.setDead();//kills the entity, meaning it wont be spawning in the world to get picked up
+			}
+			else System.out.println("out of room");
 		}
 	}
 }
