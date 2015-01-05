c1d2e5f6bfca4aeb828e3f43ade67a234f90d56d
Sam Bassett <lothrazar@hotmail.com>
2015-01-05 12:58:47 -0800

    home/worldhome : implement icommand interface

--- src/main/java/com/lothrazar/command/CommandHome.java
+++ src/main/java/com/lothrazar/command/CommandHome.java
@@ -1,31 +1,98 @@
 package com.lothrazar.command;
 
-public class CommandHome
+import java.util.List;
+
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+
+public class CommandHome implements ICommand
 {
 
-	//go to my beds spawn point
-	//in the player class there is a
-	//var c = entityPlayer.getBedLocation(0);//0 is overworld
-	
-	//this might be null
-	
-	//then we say, is a bed really there
-	//Block block = world.getBlock(c.posX, c.posY, c.posZ);
-	//if (block.equals(Blocks.bed) || block.isBed(world, c.posX, c.posY, c.posZ, null))
-	//then we ask the bed where we should spawn near it /
-	//TP TO:  block.getBedSpawnPosition(world, c.posX, c.posY, c.posZ, null);
-	
-	
-	//and for block.getMaterial().isSolid;
-	//if its solid then go up by y+1
-	
-	
-	
-	//or an alternate way, check if stuck in wall
-	/*
-	 while (!world.getCollidingBoundingBoxes(p, p.boundingBox).isEmpty())
-	 {
-		 entityPlayer.setPositionAndUpdate(p.posX, p.posY + 1.0D, p.posZ);
-	 }
-	 */
+	@Override
+	public int compareTo(Object arg0)
+	{ 
+		return 0;
+	}
+
+	@Override
+	public String getCommandName()
+	{ 
+		return "home";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender ic)
+	{ 
+		return "home";
+	}
+
+	@Override
+	public List getCommandAliases()
+	{ 
+		return null;
+	}
+
+	@Override
+	public void processCommand(ICommandSender ic, String[] args)
+	{
+
+		
+		
+		//go to my beds spawn point
+		//in the player class there is a
+		//var c = entityPlayer.getBedLocation(0);//0 is overworld
+		
+		//this might be null
+		
+		//then we say, is a bed really there
+		//Block block = world.getBlock(c.posX, c.posY, c.posZ);
+		//if (block.equals(Blocks.bed) || block.isBed(world, c.posX, c.posY, c.posZ, null))
+		//then we ask the bed where we should spawn near it /
+		//TP TO:  block.getBedSpawnPosition(world, c.posX, c.posY, c.posZ, null);
+		
+		
+		//and for block.getMaterial().isSolid;
+		//if its solid then go up by y+1
+		
+		
+		
+		//or an alternate way, check if stuck in wall
+		/*
+		 while (!world.getCollidingBoundingBoxes(p, p.boundingBox).isEmpty())
+		 {
+			 entityPlayer.setPositionAndUpdate(p.posX, p.posY + 1.0D, p.posZ);
+		 }
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender ic)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender ic, String[] args)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] args, int i)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
 }
--- src/main/java/com/lothrazar/command/CommandWorldHome.java
+++ src/main/java/com/lothrazar/command/CommandWorldHome.java
@@ -1,10 +1,66 @@
 package com.lothrazar.command;
 
-public class CommandWorldHome
+import java.util.List;
+
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+
+public class CommandWorldHome  implements ICommand
 {
 
-	//worldhome to go to the worlds spawnpoint
-	
-	//world.getSpawnPoint();
-//
+	@Override
+	public int compareTo(Object o)
+	{ 
+		return 0;
+	}
+
+	@Override
+	public String getCommandName()
+	{ 
+		return "worldhome";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender ic)
+	{ 
+		return "worldhome";
+	}
+
+	@Override
+	public List getCommandAliases()
+	{ 
+		return null;
+	}
+
+	@Override
+	public void processCommand(ICommandSender ic, String[] args)
+	{ 
+
+		//worldhome to go to the worlds spawnpoint
+		
+		//world.getSpawnPoint();
+	//
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender ic)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender ic, String[] args)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] ic, int args)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
