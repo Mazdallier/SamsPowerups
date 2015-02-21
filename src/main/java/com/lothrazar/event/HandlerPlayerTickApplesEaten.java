62937ac533f75946cc59efbf90b1c5e0807bb37b
Lothrazar <samson.bassett@gmail.com>
2015-02-21 15:13:54 -0800

    apple effects: refactor the paths into a method for each, tickHearts and tickFlying. pass player instead of whole event obj

--- src/main/java/com/lothrazar/event/HandlerPlayerTickApplesEaten.java
+++ src/main/java/com/lothrazar/event/HandlerPlayerTickApplesEaten.java
@@ -9,6 +9,7 @@ import com.lothrazar.item.ItemFoodAppleMagic.MagicType;
 import com.lothrazar.util.Reference;
 import com.lothrazar.util.SamsUtilities; 
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.common.config.Configuration; 
 import net.minecraft.world.World;   
@@ -33,76 +34,86 @@ public class HandlerPlayerTickApplesEaten
 	public void onPlayerTick(PlayerTickEvent event)
 	{    	 
 		if( event.player.capabilities.isCreativeMode){return;}//leave flying and hearts and stuff alone
+		
 		if( event.player.worldObj.isRemote  == false )
 		{ 	
-			int countApplesEaten = SamsUtilities.getPlayerIntegerNBT(event.player, Reference.MODID + MagicType.Hearts.toString());
-			
-			int healthBoostLevel = countApplesEaten - 1; //you get 2 red hearts per level
-			
-			if(healthBoostLevel >= 0  && 
-			   event.player.isPotionActive(Reference.potion_HEALTH_BOOST) == false)
-			{ 
-				//so we have eaten at least one apple, and the potion effect has been cleared, so we apply it
-				int duration = 60 * 60 * Reference.TICKS_PER_SEC;
-				event.player.addPotionEffect(new PotionEffect(Reference.potion_HEALTH_BOOST, duration, healthBoostLevel,false,false)); 
-			}
-			 
+			tickHearts(event.player); 
 		}
 		else //isRemote == true
 		{ 	
-			//whenever we eat a nether apple, we are given a bunch of 'flying  ticks' that add up
-			int countAppleTicks = SamsUtilities.getPlayerIntegerNBT(event.player, Reference.MODID + MagicType.Flying.toString());
-			 
-			//first, check are we allowed to fly
-			if (countAppleTicks > 0)
-			{ 
-				event.player.capabilities.allowFlying = true;   
-			} 
-			else
-			{  
-				// disable flying now & in future
-				event.player.capabilities.allowFlying = false;  
-				event.player.capabilities.isFlying = false;  
-			}
-			
-			//now check, are we currently flying right now
-			if (event.player.capabilities.isFlying)
-			{   
-				//every tick that we fly uses up one resource tick that was given by the apple (hence the -1_
-				SamsUtilities.incrementPlayerIntegerNBT(event.player, Reference.MODID + MagicType.Flying.toString(), -1);
+			tickFlying(event.player);  
+		}  
+	}
 
-				int level = 4;//no number is actually default, so this makes potion effect 2 == III, 4 == V
-				int duration = 2 * Reference.TICKS_PER_SEC;  //2 seconds
-				
-				if(doesWeakness) 
-					event.player.addPotionEffect(new PotionEffect(Reference.potion_FATIGUE, duration, level));
-				 
-				if(doesFatigue) 
-					event.player.addPotionEffect(new PotionEffect(Reference.potion_WEAKNESS, duration, level)); 
-				  
-				if(doesDrainHunger)  
-					event.player.addPotionEffect(new PotionEffect(Reference.potion_HUNGER, duration, 0));
-				 
-				    
-			} // end if isFlying
-			else //so therefore isFlying is false
+	private void tickFlying(EntityPlayer player) 
+	{
+		//whenever we eat a nether apple, we are given a bunch of 'flying  ticks' that add up
+		int countAppleTicks = SamsUtilities.getPlayerIntegerNBT(player, Reference.MODID + MagicType.Flying.toString());
+		 
+		//first, check are we allowed to fly
+		if (countAppleTicks > 0)
+		{ 
+			player.capabilities.allowFlying = true;   
+		} 
+		else
+		{  
+			// disable flying now & in future
+			player.capabilities.allowFlying = false;  
+			player.capabilities.isFlying = false;  
+		}
+		
+		//now check, are we currently flying right now
+		if (player.capabilities.isFlying)
+		{   
+			//every tick that we fly uses up one resource tick that was given by the apple (hence the -1_
+			SamsUtilities.incrementPlayerIntegerNBT(player, Reference.MODID + MagicType.Flying.toString(), -1);
+
+			int level = 4;//no number is actually default, so this makes potion effect 2 == III, 4 == V
+			int duration = 2 * Reference.TICKS_PER_SEC;  //2 seconds
+			
+			if(doesWeakness) 
+				player.addPotionEffect(new PotionEffect(Reference.potion_FATIGUE, duration, level));
+			 
+			if(doesFatigue) 
+				player.addPotionEffect(new PotionEffect(Reference.potion_WEAKNESS, duration, level)); 
+			  
+			if(doesDrainHunger)  
+				player.addPotionEffect(new PotionEffect(Reference.potion_HUNGER, duration, 0));
+			 
+			    
+		} // end if isFlying
+		else //so therefore isFlying is false
+		{  
+			if (player.posY < player.prevPosY) // we are falling 
 			{  
-				if (event.player.posY < event.player.prevPosY) // we are falling 
-				{  
-					event.player.capabilities.allowFlying = false;// to enable  fall distance
-	
-					//erase the potion effects , dont just let them tick down
-					//(in case it bugs out-  leave them lingering with 0:00 potions forever )
-					if(doesWeakness)
-						 event.player.removePotionEffect(Reference.potion_FATIGUE);
-					
-					if(doesFatigue)
-						 event.player.removePotionEffect(Reference.potion_WEAKNESS);
-					 
-					if(doesDrainHunger) 
-						event.player.removePotionEffect(Reference.potion_HUNGER); 
-				} 
-			}  
-		} //end if else branch on isRemote
+				player.capabilities.allowFlying = false;// to enable  fall distance
+
+				//erase the potion effects , dont just let them tick down
+				//(in case it bugs out-  leave them lingering with 0:00 potions forever )
+				if(doesWeakness)
+					 player.removePotionEffect(Reference.potion_FATIGUE);
+				
+				if(doesFatigue)
+					 player.removePotionEffect(Reference.potion_WEAKNESS);
+				 
+				if(doesDrainHunger) 
+					player.removePotionEffect(Reference.potion_HUNGER); 
+			} 
+		}
+	}
+
+	private void tickHearts(EntityPlayer player) 
+	{
+		int countApplesEaten = SamsUtilities.getPlayerIntegerNBT(player, Reference.MODID + MagicType.Hearts.toString());
+		
+		int healthBoostLevel = countApplesEaten - 1; //you get 2 red hearts per level
+		
+		if(healthBoostLevel >= 0  && 
+		   player.isPotionActive(Reference.potion_HEALTH_BOOST) == false)
+		{ 
+			//so we have eaten at least one apple, and the potion effect has been cleared, so we apply it
+			int duration = 60 * 60 * Reference.TICKS_PER_SEC;
+			player.addPotionEffect(new PotionEffect(Reference.potion_HEALTH_BOOST, duration, healthBoostLevel,false,false)); 
+		}
 	}
 }
