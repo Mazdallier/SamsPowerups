7a278b4eb24b6addc936bc2fe7ce3fb4c51a42d4
Lothrazar <samson.bassett@gmail.com>
2015-03-02 18:53:01 -0800

    event handler hooked up for bucket storage

---
+++ src/main/java/com/lothrazar/event/HandlerBucketStorage.java
@@ -0,0 +1,30 @@
+package com.lothrazar.event;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.lothrazar.item.ItemBucketStorage;
+import com.lothrazar.item.ItemWandTransform;
+
+public class HandlerBucketStorage 
+{
+
+	@SubscribeEvent
+	public void onPlayerInteract(PlayerInteractEvent event)
+  	{      
+		if(event.world.isRemote){ return ;}//server side only!
+		ItemStack held = event.entityPlayer.getCurrentEquippedItem();  
+		if(held == null) { return; }//empty hand so do nothing
+		  
+		Block blockClicked = event.entityPlayer.worldObj.getBlockState(event.pos).getBlock();
+		   
+		if(held.getItem() == ItemBucketStorage.itemLava && 
+				event.action.RIGHT_CLICK_BLOCK == event.action)
+		{
+			ItemBucketStorage.placeLiquid(event.entityPlayer, held, event.pos);
+		}
+	
+  	}
+}
--- src/main/java/com/lothrazar/item/ItemBucketStorage.java
+++ src/main/java/com/lothrazar/item/ItemBucketStorage.java
@@ -4,9 +4,11 @@ import com.lothrazar.samscontent.ModLoader;
 import com.lothrazar.util.SamsRegistry;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemBucketStorage  extends Item
@@ -32,9 +34,10 @@ public class ItemBucketStorage  extends Item
 				Items.lava_bucket, Items.lava_bucket, Items.lava_bucket,
 				Items.lava_bucket, Items.lava_bucket, Items.lava_bucket,
 				Items.lava_bucket, Items.lava_bucket, Items.lava_bucket);
- 
+ /*
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.lava_bucket,9), 
 				itemLava);
+				*/
 	}
 	public static ItemBucketStorage itemWater;
 	public static void initWater()
@@ -48,26 +51,32 @@ public class ItemBucketStorage  extends Item
 				Items.water_bucket, Items.water_bucket, Items.water_bucket,
 				Items.water_bucket, Items.water_bucket, Items.water_bucket,
 				Items.water_bucket, Items.water_bucket, Items.water_bucket);
- 
+ /*
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.water_bucket,9), 
 				itemWater);
+				*/
 	}
 	public static ItemBucketStorage itemMilk;
 	public static void initMilk()
 	{
 		//if(!ModLoader.settings.enderBook) {return;}//TODO: config
-		itemWater = new ItemBucketStorage();
+		itemMilk = new ItemBucketStorage();
 
 		SamsRegistry.registerItem(itemMilk, "bucket_storage_milk");
-
+ 
 		GameRegistry.addShapelessRecipe(new ItemStack(itemMilk), 
 				Items.milk_bucket, Items.milk_bucket, Items.milk_bucket,
 				Items.milk_bucket, Items.milk_bucket, Items.milk_bucket,
 				Items.milk_bucket, Items.milk_bucket, Items.milk_bucket);
- 
+		//TODO: using it will place one spot of water and use up one durability
+		//the 9x9 at once doesnt make sense
+ /*
 		GameRegistry.addShapelessRecipe(new ItemStack(Items.milk_bucket,9), 
 				itemMilk);
+				*/
 	}
- 
-	
+	public static void placeLiquid(EntityPlayer entityPlayer, ItemStack held,BlockPos pos) 
+	{
+//TODO: 	
+	} 
 }
