562980769f22c337296173bad456b207fd5f3c97
Lothrazar <samson.bassett@gmail.com>
2015-03-09 01:32:58 -0700

    wand copy paste: extracted text from tile. simple read write for copy paste

--- src/main/java/com/lothrazar/item/ItemWandCopyPaste.java
+++ src/main/java/com/lothrazar/item/ItemWandCopyPaste.java
@@ -13,9 +13,12 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -36,6 +39,29 @@ public class ItemWandCopyPaste  extends Item
 
 		SamsRegistry.registerItem(ItemRegistry.wandCopy, "wand_copy");
   
+	}
+
+	public static void copySign(World world, EntityPlayer entityPlayer,	TileEntitySign sign, ItemStack held) 
+	{ 
+		SamsUtilities.setItemStackNBT(held, "sign_0", sign.signText[0].getUnformattedText());
+		SamsUtilities.setItemStackNBT(held, "sign_1", sign.signText[1].getUnformattedText());
+		SamsUtilities.setItemStackNBT(held, "sign_2", sign.signText[2].getUnformattedText());
+		SamsUtilities.setItemStackNBT(held, "sign_3", sign.signText[3].getUnformattedText());
+
+		entityPlayer.swingItem();
+	}
+
+	public static void pasteSign(World world, EntityPlayer entityPlayer,	TileEntitySign sign, ItemStack held) 
+	{
+
+		sign.signText[0] = new ChatComponentText(SamsUtilities.getItemStackNBT(held, "sign_0"));
+		sign.signText[1] = new ChatComponentText(SamsUtilities.getItemStackNBT(held, "sign_1"));
+		sign.signText[2] = new ChatComponentText(SamsUtilities.getItemStackNBT(held, "sign_2"));
+		sign.signText[3] = new ChatComponentText(SamsUtilities.getItemStackNBT(held, "sign_3"));
+
+		entityPlayer.swingItem();
+	 
+		
 	}
  
 	 
